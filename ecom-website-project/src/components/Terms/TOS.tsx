a1ebdae278bc25da0cea0f7551601a08e1cdd995
Thiago de Matos <thiago.dematos.1@gmail.com>
2024-04-17 20:40:56 -0400

    created several component pages: TOS, Privacy Policy, and Shopping Cart

---
+++ ecom-website-project/src/components/Terms/TOS.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TermsOfService: React.FC = () => {
+  return <div></div>;
+};
+
+export default TermsOfService;
---
+++ ecom-website-project/src/components/shop components/ShoppingCart.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ShoppingCart: React.FC = () => {
+  return <div></div>;
+};
+
+export default ShoppingCart;
