e44711fe8d20e607534e507300d493368e754403
Thiago de Matos <thiago.dematos.1@gmail.com>
2024-04-26 08:11:00 -0400

    [registration] - add nav links via components

--- ecom-website-project/src/components/login components/Account.tsx
+++ ecom-website-project/src/components/login components/Account.tsx
@@ -1,14 +1,30 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import { Outlet } from "../../../node_modules/react-router-dom/dist/index";
 import NavBar from "../Navigation";
 import Footer from "../Footer";
+import NavSelector from "../NavSelector";
 import "../../style/login.css";
 
 const Account: React.FC = () => {
+  const [toggleModal, setToggleModal] = useState<boolean>(false);
+
+  const handleMouseOver = () => {
+    setToggleModal(true);
+  };
+
+  const handleMouseOut = () => {
+    setToggleModal(false);
+  };
+
   return (
     <Fragment>
-      <NavBar />
-
+      <NavBar about="ABOUT" shop="SHOP" onMouseOver={handleMouseOver} />
+      <NavSelector
+        navTitle="INVENTORY"
+        caption="Not sure where to start?"
+        isDialogOpen={toggleModal}
+        onMouseLeave={handleMouseOut}
+      />
       <Outlet />
       <Footer />
     </Fragment>
