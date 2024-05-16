364ad248a7d98c5637a9b2a58e35b528bf439e88
Thiago de Matos <thiago.dematos.1@gmail.com>
2024-05-15 20:58:11 -0400

    [about] - add body section of about page, add styling, and add functionality to nav menu

--- ecom-website-project/src/components/About.tsx
+++ ecom-website-project/src/components/About.tsx
@@ -1,9 +1,45 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
+import NavBar from "./Navigation";
+import Footer from "./Footer";
+import styles from "../style/about.module.css";
+import NavSelector from "./NavSelector";
 
 const About: React.FC = () => {
+  const [toggleModal, setToggleModal] = useState<boolean>(false);
+  const [UCToggle, setUCToggle] = useState<boolean>(true);
+
+  const handleMouseOver = () => {
+    setToggleModal(true);
+  };
+
+  const handleMouseOut = () => {
+    setToggleModal(false);
+  };
+
   return (
     <Fragment>
-      <h1>About</h1>
+      <NavBar about="ABOUT" shop="SHOP" onMouseOver={handleMouseOver} />
+      <NavSelector
+        navTitle="INVENTORY"
+        caption="Not sure where to start?"
+        isDialogOpen={toggleModal}
+        onMouseLeave={handleMouseOut}
+      />
+      <div className={styles["aboutPagecontainer"]}>
+        <div className={styles["aboutPageBox"]}>
+          <div className={styles["aboutUsImage"]}></div>
+          <h3 className={styles["aboutUSTitle"]}>About Us</h3>
+          <h4 className={styles["aboutUSSubTitle"]}>
+            Interested in knowing what we do?
+          </h4>
+          <p className={styles["aboutUSDescription"]}>
+            This is a website that is designated in selling the best of the best
+            video games. And... No! We do NOT sell consoles, we only sell the
+            games, the juice, the fun.... We are your ONE STOP SHOP!
+          </p>
+        </div>
+      </div>
+      <Footer />
     </Fragment>
   );
 };
