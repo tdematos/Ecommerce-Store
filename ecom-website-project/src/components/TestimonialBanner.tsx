42514e897dcaa82b5760519ebb41dee65180046c
Thiago de Matos <thiago.dematos.1@gmail.com>
2024-06-14 12:57:55 -0400

    [testimonial banner] - add ability to cycle through index of review file and gave functionality to press arrow and toggle through reviews dynamically on the page

--- ecom-website-project/src/components/TestimonialBanner.tsx
+++ ecom-website-project/src/components/TestimonialBanner.tsx
@@ -1,30 +1,59 @@
-import React, { Fragment } from "react";
+import React, { useState } from "react";
 import "../style/testimonialBanner.css";
 import { reviews } from "./Reviews";
 
 const TestimonialBanner: React.FC = () => {
+  const [currentTestimonialIndex, setCurrentTestimonialIndex] = useState(0);
+
+  const handleRightArrowClick = () => {
+    setCurrentTestimonialIndex((prevIndex) => (prevIndex + 1) % reviews.length);
+  };
+
+  const handleLeftArrowClick = () => {
+    setCurrentTestimonialIndex(
+      (prevIndex) => (prevIndex - 1 + reviews.length) % reviews.length
+    );
+  };
+
+  const handleBallClick = (index: number) => {
+    setCurrentTestimonialIndex(index);
+  };
+
   return (
-    <Fragment>
-      <section className="customer-testimonials">
-        <div>
-          <button className="t-left arrow">&lt;</button>
+    <section className="customer-testimonials">
+      <div>
+        <button className="t-left arrow" onClick={handleLeftArrowClick}>
+          &lt;
+        </button>
+      </div>
+      <div className="testimonial-container">
+        <h3 className="testimonial-title">Happy Customers</h3>
+        <p className="written-testimonial">
+          {reviews[currentTestimonialIndex] &&
+            reviews[currentTestimonialIndex].review}
+        </p>
+        <p className="customer-name">
+          {reviews[currentTestimonialIndex] &&
+            reviews[currentTestimonialIndex].person}
+        </p>
+        <div className="t-selector">
+          {reviews.map((_, index) => (
+            <div
+              key={index}
+              className={`round-select ${
+                index === currentTestimonialIndex ? "active" : ""
+              }`}
+              onClick={() => handleBallClick(index)}
+            ></div>
+          ))}
         </div>
-        <div className="testimonial-container">
-          <h3 className="testimonial-title">Happy Customers</h3>
-          <p className="written-testimonial">
-            {reviews[0] && reviews[0].review}
-          </p>
-          <p className="customer-name">{reviews[0] && reviews[0].person}</p>
-          <div className="t-selector">
-            <div className="round-select"></div>
-            <div className="round-select"></div>
-          </div>
-        </div>
-        <div>
-          <button className="t-right arrow">&gt;</button>
-        </div>
-      </section>
-    </Fragment>
+      </div>
+      <div>
+        <button className="t-right arrow" onClick={handleRightArrowClick}>
+          &gt;
+        </button>
+      </div>
+    </section>
   );
 };
 
